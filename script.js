cc6cb92a793cbf91ba202bd17835ce3acf4adac4
rishitasadhu24 <rishitasadhu24@gmail.com>
2026-05-14 11:55:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function showMessage() {
+function showMessage(){
 
     alert("Thank you for choosing Brew Haven Café!");
 
 }
-
-document
-.querySelector(".contact-form")
-.addEventListener("submit", function(e){
-
-    e.preventDefault();
-
-    alert("Message sent successfully!");
-
-});
